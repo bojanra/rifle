8f945940e30b9448f8e88ed566c25df439f45829
ramsakb <bojan.ramsak@rtvslo.si>
2015-11-02 14:01:25 +0100

    radioamater version with modified control protocol

--- rifle.c
+++ rifle.c
@@ -2,56 +2,40 @@
   Rifle - antenna rotator interface for YAESU G-1000DXC series rotator
 
   -----------------------------------------------------------------------------
-  2012/7/11 - 2013/10/28 - 20141/24 - 2014/11/27 
+  2012/7/11 - 2013/10/28 - 2014/1/24 - 2014/4/7
 
-Basically all work is build around the timer wich is 4 times the baudrate and 
+
+Basically all work is build around the timer wich is 4 times the baudrate and
 implements a software uart. The target is ATTINY 26.
 
 In normal mode the Led is blinking Duty Cyle 10%.
-When incrementing speed for rotator A the DC is 10-10%;
 
-We can control 2 yaesu rotators A and B. 
+We can control 2 yaesu rotators A and B.
 
-When more than (60 seconds) DEADTIME after each start of movement the target is not reached 
-we stop the rotator movement.
+Serial connection is 9600 8n1
+
+When more than (120 seconds) DEADTIME after each start of movement the target is not reached
+the movement is stopped.
 
 The watchdog is active via the main loop.
 
 Accept these commands:
-g - get readings:
-    bearing device A,
-    bearing device B,
-    fieldstrength voltage (mosley),
-    rotator_status
-    (g150.234.001.0\n) 3 digits rotator A,3 digits rotator B,3 digits voltage,1 digit rotator_status
+(command termination is 0x0d)
+C2 - get readings in form +0aaa+0eee
+Waaa eee - set destination and start  W200 060
+S - stop motion & tuning mode & debug output (space)
 
-    Legend for rotator_status:
-    0 - stopped
-    1 - b moving
-    2 - a moving
-    3 - a and b moving
-
+(no command termination - direct reading)
 t - tuning mode on - continously send readings
 T - stop tuning mode
 r - reset via watchdog
-
-axxx\r - set bearing device A
-bxxx\r - set bearing device B
-
 v - Version string
-  - stop motion & tuning mode & debug output (space)
 
-Additional feature antena death protection. Stop rotator movement after 60 seconds.
-
-Some modifications for ATTINY 261A - change of register names and watchdog problems
-
- *****************************************************************************/
-// internal clock 
+*****************************************************************************/
+// internal clock
 #define F_CPU 8000000
-#define TIMER0PRESET (255-98)   //  255-t_bit  ,  t_bit = 1/2400/4*F_CPU/prescaler 
-#define DEADTIME 60             // after DEADTIME seconds since start of movement the rotator is stopped
-
-#define DEBUG 1
+#define TIMER0PRESET 83         // preset for timer 0 - responsible for sampling rate -> baudrate
+#define DEADTIME 120            // after DEADTIME seconds since start of movement the rotator is stopped
 
 #include <avr/io.h>
 // #include <util/delay.h>
@@ -59,27 +43,24 @@ Some modifications for ATTINY 261A - change of register names and watchdog probl
 #include <avr/pgmspace.h>
 #include <avr/wdt.h>
 
-const char version[] PROGMEM = "Rifle V4.6\n";
+const char version[] PROGMEM = "Rifle V5.2\n";
 
 #define	UARTTX      PA7
 #define UARTRX      PA6
 #define LED         PA3
 #define MOSL        PA1   // input for received fieldstrength
 
-#define LEFT_A      PB0   // rotator A
-#define RIGHT_A     PB1    
+#define LEFT_A      PB0   // rotator A - azimuth
+#define RIGHT_A     PB1
 #define SPEEDCTRL_A PB3
-#define SPEED_A     OCR1B
 #define BEAR_A      PA0   // input for rotator A bearing
 
-
-#define LEFT_B      PA4   // rotator B
+#define LEFT_B      PA4   // rotator B - elevation 0-90
 #define RIGHT_B     PA5
 #define SPEEDCTRL_B PB6
 #define BEAR_B      PA2
 
 #define ADC_MUX_BEAR_A  0   // read bearing from port PA0
-#define ADC_MUX_MOSL    1   // analog input from port PA1
 #define ADC_MUX_BEAR_B  2   // read bearing from port PA2
 
 static volatile uint8_t   seconds;
@@ -100,14 +81,13 @@ static volatile uint16_t  differ;
 #define job_stop_motion   4
 #define job_get_reading   8
 #define job_tuning        16
-#define job_timeout       32
 
 // status codes
 #define moving_a    1
 #define moving_b    2
 
 // clock times
-static volatile uint8_t ticks;			// 192 of these make 1/50th of a second
+static volatile uint16_t ticks;			// 192 of these make 1/50th of a second
 static volatile uint8_t fiftieths;
 
 // software uart registers
@@ -120,22 +100,21 @@ static uint8_t	uart_rxbit;		      // bit count for rx
 static volatile uint8_t	uart_status;// uart status
 static volatile uint8_t next_write;	// the pointers to the receive2 buffer
 
-static unsigned char buffer[8]; // circular buffer for receiver
+static unsigned char buffer[16]; // circular buffer for receiver
 
 // status bits
 #define txbusy 0				// set if a byte is in transmission
 #define rxbusy 1				// set if a byte is being received
 #define rxframe 2				// set if the stop byte isn't a one
 
-#define ABSDIFF(a, b) ((a) < (b)? ((b) - (a)): ((a) - (b))) 
+#define ABSDIFF(a, b) ((a) < (b)? ((b) - (a)): ((a) - (b)))
 
-
-ISR( TIMER0_OVF_vect) {
+ISR( TIMER0_OVF0_vect) {
   // the interrupt signal
 
   // we arrive here four times per baud rate
   // preset the timer0
-  TCNT0L = TIMER0PRESET;				// preset for timer
+  TCNT0 = TIMER0PRESET;				// preset for timer
 
   // we first send the output signal, if there's anything to send,
   // since it needs to be somewhere close to accurate...
@@ -240,7 +219,7 @@ ISR( TIMER0_OVF_vect) {
           // and set the higest bit
           // if the data bit is a one, set we add the bit value to the rxd value
           if ((PINA & (1<<UARTRX)) != 0) {
-            uart_rxd |= (1<<7); 
+            uart_rxd |= (1<<7);
           }
           uart_rxbit ++;
         }
@@ -255,23 +234,9 @@ ISR( TIMER0_OVF_vect) {
 
           // finished, ready to start again
           uart_status &= ~(1<<rxbusy);
-          // test the received character
-          if( uart_rxd == ' ') {
-            // stop all
-            rotator_status = 0;
-            job = 0;
-            PORTB &= ~(1<<LEFT_A);
-            PORTB &= ~(1<<RIGHT_A);
-            PORTA &= ~(1<<LEFT_B);
-            PORTA &= ~(1<<RIGHT_B);
-            next_write = 0;
-          }
-          else if( uart_rxd == 'v') {
+          // TEST THE RECEIVED CHARACTER
+          if( uart_rxd == 'v') {
             job |= job_getversion;
-            next_write = 0;
-          }
-          else if( uart_rxd == 'g') {
-            job |= job_get_reading;
             next_write = 0;
           }
           else if( uart_rxd == 'T') {
@@ -279,6 +244,7 @@ ISR( TIMER0_OVF_vect) {
             next_write = 0;
           }
           else if( uart_rxd == 'r' ) {
+            // reset
             while( 1) {
             }
           }
@@ -286,65 +252,68 @@ ISR( TIMER0_OVF_vect) {
             job |= job_tuning;
             next_write = 0;
           }
-          else if( uart_rxd == 'a' || uart_rxd == 'b' ) {
-            next_write = 0;
-            buffer[next_write++] = uart_rxd;
-          }
-          else if( uart_rxd == 13 && next_write == 4) {
-            if( buffer[1] >= '0' && buffer[1] <= '9' &&
-                buffer[2] >= '0' && buffer[2] <= '9' &&
-                buffer[3] >= '0' && buffer[3] <= '9' ) {
+          else if( uart_rxd == 13) {
+            // command W
+            if( next_write == 8 && buffer[0] == 'W' && buffer[4] == ' ') {
               uint16_t target;
               target = 100*(buffer[1] & 0x0f)+10*(buffer[2] & 0x0f)+(buffer[3] & 0x0f);
-              if( buffer[0] == 'a') {
-                target_a = target;
-                if( target_a <= 450) {
-                  // target is o.k. start moving //
-                  SPEED_A = 0;
-                  if( target_a > bearing_a) {
-                    // right
-                    PORTB &= ~(1<<LEFT_A);
-                    PORTB |= (1<<RIGHT_A);
-                  }
-                  else if( target_a < bearing_a) {
-                    // left
-                    PORTB &= ~(1<<RIGHT_A);
-                    PORTB |= (1<<LEFT_A);
-                  }
-                  seconds = 0;
-                  rotator_status |= moving_a;
-                }
-                else {
-                  // stop moving //
-                  rotator_status &= ~moving_a;
-                  PORTB &= ~(1<<RIGHT_A);
+              target_a = target;
+              if( target_a <= 450) {
+                // target is o.k. start moving //
+                if( target_a > bearing_a) {
+                  // right
                   PORTB &= ~(1<<LEFT_A);
+                  PORTB |= (1<<RIGHT_A);
                 }
+                else if( target_a < bearing_a) {
+                  // left
+                  PORTB &= ~(1<<RIGHT_A);
+                  PORTB |= (1<<LEFT_A);
+                }
+                seconds = 0;
+                rotator_status |= moving_a;
               }
-              else if( buffer[0] == 'b') {
-                target_b = target;
-                if( target_b <= 450) {
-                  // target is o.k. start moving //
-                  if( target_b > bearing_b) {
-                    // right
-                    PORTA &= ~(1<<LEFT_B);
-                    PORTA |= (1<<RIGHT_B);
-                  }
-                  else if( target_b < bearing_b) {
-                    // left
-                    PORTA &= ~(1<<RIGHT_B);
-                    PORTA |= (1<<LEFT_B);
-                  }
-                  seconds = 0;
-                  rotator_status |= moving_b;
-                }
-                else {
-                  // stop moving //
-                  rotator_status &= ~moving_b;
-                  PORTA &= ~(1<<RIGHT_B);
+              else {
+                // stop moving //
+                rotator_status &= ~moving_a;
+                PORTB &= ~(1<<RIGHT_A);
+                PORTB &= ~(1<<LEFT_A);
+              }
+              target = 100*(buffer[5] & 0x0f)+10*(buffer[6] & 0x0f)+(buffer[7] & 0x0f);
+              target_b = target;
+              if( target_b <= 150) {
+                // target is o.k. start moving //
+                if( target_b > bearing_b) {
+                  // right
                   PORTA &= ~(1<<LEFT_B);
+                  PORTA |= (1<<RIGHT_B);
                 }
+                else if( target_b < bearing_b) {
+                  // left
+                  PORTA &= ~(1<<RIGHT_B);
+                  PORTA |= (1<<LEFT_B);
+                }
+                seconds = 0;
+                rotator_status |= moving_b;
               }
+              else {
+                // stop moving //
+                rotator_status &= ~moving_b;
+                PORTA &= ~(1<<RIGHT_B);
+                PORTA &= ~(1<<LEFT_B);
+              }
+            }
+            else if( next_write == 1 && buffer[0] == 'S') {
+              // command S - stop all
+              rotator_status = 0;
+              job = 0;
+              PORTB &= ~(1<<LEFT_A);
+              PORTB &= ~(1<<RIGHT_A);
+              PORTA &= ~(1<<LEFT_B);
+              PORTA &= ~(1<<RIGHT_B);
+            }
+            else if( next_write == 2 && buffer[0] == 'C' && buffer[1] == '2') {
+              job |= job_get_reading;
             }
             next_write = 0;
           }
@@ -352,7 +321,7 @@ ISR( TIMER0_OVF_vect) {
             // store the data into the buffer
             buffer[next_write++] = uart_rxd;
             // buffer protection
-            next_write &= 0x07;
+            next_write &= 0x0f;
           }
         }
       }
@@ -361,46 +330,44 @@ ISR( TIMER0_OVF_vect) {
 
   // increment the clock
   ticks++;
-  if( ticks == 192) {
+  if( ticks == 768) {
     // run this code every 20ms
     ticks = 0;
     fiftieths++;
 
     // ADC stuff
-    if( (ADCSRA & (1<<ADSC)) == 0) {
+    if( (ADCSR & (1<<ADSC)) == 0) {
       if( ADMUX == 0) {
-        // read bearing a
+        // read bearing a - azimuth
         bearing_a = ADCW;
         bearing_a >>= 1;
-        ADMUX = ADC_MUX_MOSL;
-      }
-      else if( ADMUX & ADC_MUX_MOSL) {
-        // read mosley
-        mosley = ADCW;
-        mosley >>= 2;
         ADMUX = ADC_MUX_BEAR_B;
       }
       else {
-        // read bearing b
+        // read bearing b - elevation
         bearing_b = ADCW;
-        bearing_b >>= 1;
+        bearing_b >>= 2;   // and divide by 2
+        if( bearing_b >= 90)
+          bearing_b -= 90; // offset because of "incorrect" mounting of unit
+        else
+          bearing_b = 0;
         ADMUX = ADC_MUX_BEAR_A;
       }
-      ADCSRA |= (1<<ADSC);        // start AD conversion
+      ADCSR |= (1<<ADSC);        // start AD conversion
     }
 
     // movement control - do not overshoot - only check if moving
     if( (fiftieths % 10 == 0) && rotator_status) {
       // we have arrived at the target bearing
-      if( target_a == bearing_a || 
-          ((PORTB & (1<<LEFT_A)) && target_a > bearing_a) || 
+      if( target_a == bearing_a ||
+          ((PORTB & (1<<LEFT_A)) && target_a > bearing_a) ||
           ((PORTB & (1<<RIGHT_A)) && target_a < bearing_a)) {
         rotator_status &= ~moving_a;
         PORTB &= ~(1<<RIGHT_A);
         PORTB &= ~(1<<LEFT_A);
       }
-      if( target_b == bearing_b || 
-          ((PORTA & (1<<LEFT_B)) && target_b > bearing_b) || 
+      if( target_b == bearing_b ||
+          ((PORTA & (1<<LEFT_B)) && target_b > bearing_b) ||
           ((PORTA & (1<<RIGHT_B)) && target_b < bearing_b)) {
         rotator_status &= ~moving_b;
         PORTA &= ~(1<<RIGHT_B);
@@ -435,7 +402,6 @@ ISR( TIMER0_OVF_vect) {
         PORTB &= ~(1<<RIGHT_A);
         PORTA &= ~(1<<LEFT_B);
         PORTA &= ~(1<<RIGHT_B);
-        job ^= job_timeout;
         rotator_status = 0;
       }
       if( seconds == 255) { seconds = DEADTIME;}
@@ -485,15 +451,14 @@ void init( void) {
   PORTA |= 1<<UARTTX | 1<<LED;	                            	        // write 1 to output
   PORTB |= 1<<SPEEDCTRL_A | 1<<SPEEDCTRL_B;               		        // write 1 to output
 
-  TCCR0B = 0x02;               // set prescaler for timer0 to 8
-  TCNT0L = TIMER0PRESET;				// preset for timer
+  TCCR0 = 0x01;               // set prescaler for timer0 to 1
+  TCNT0 = TIMER0PRESET;				// preset for timer
 
   TIMSK = (1<<TOIE0); 		    // allow interrupts on timer0 overflow
 
 //  TCCR1A = 0x21;              // enable PWM mode for OC1B - PB3, clear PB3 on compare match. Set when TCNT=0x01
-//  TCCR1B = 0x01;              // prescaler = 1 
-//  OCR1C = 0x0a;               // the PWM counter is counting from 0 - 
-//  SPEED_A = 0;                // default is lowest rotating speed
+//  TCCR1B = 0x01;              // prescaler = 1
+//  OCR1C = 0x0a;               // the PWM counter is counting from 0 -
 
   next_write = 0;       // set up the buffer pointers
 
@@ -502,14 +467,16 @@ void init( void) {
   rotator_status = 0;     // the rotator is not moving
 
   // Configure ADC
-  ADCSRA = 0b00000110;         // Prescaler = 64 >> clk=125kHz,  Single conversion
+  //ADCSRA = 0b00100011;        // Prescaler = 8 >> clk=150kHz,  Enable auto trigger
+  ADCSR = 0b00000110;         // Prescaler = 64 >> clk=125kHz,  Single conversion
 
-  ADMUX = ADC_MUX_BEAR_A;     
-  ADCSRB = 0;                  // use Vcc as referenc and select input
+  ADMUX = ADC_MUX_BEAR_A;     // use Vcc as referenc and select input
 
-  ADCSRA |= (1<<ADEN);         // enable the ADC
+  ADCSR |= (1<<ADEN);         // enable the ADC
+  ADCSR |= (1<<ADSC);         // start AD conversion
 
-  ADCSRA |= (1<<ADSC);         // start AD conversion
+  // uint16_t result;
+  // result = ADCW;
 
   seconds = 0;                // just count seconds
   reset = 1;                  // show recovering from reset
@@ -521,8 +488,6 @@ void init( void) {
 int main(void) {
   const char* addr;
   char c;
-  MCUSR = 0;
-  wdt_disable();
 
   init();
   while( seconds < 1) {};  // wait for 1 second
@@ -531,11 +496,6 @@ int main(void) {
 
   job = job_getversion;
   while(1) {
-    if( job & job_timeout) {
-      job &= ~job_timeout;
-      put_char( 't');
-      put_char( '\n');      
-    }
     if( job & job_getversion) {
       job &= ~job_getversion;
       addr = version;
@@ -544,16 +504,12 @@ int main(void) {
       }
     }
     if( job & job_get_reading || job & job_tuning) {
-      put_char( 'g');
+      put_char( '+');
+      put_char( '0');
       put_int( bearing_a);
-      put_char( '.');
+      put_char( '+');
+      put_char( '0');
       put_int( bearing_b);
-      put_char( '.');
-      put_int( mosley+1);
-      put_char( '.');
-      c = '0';
-      c += rotator_status;
-      put_char( c);
       put_char( '\n');
       if( job & job_get_reading) {
         job &= ~job_get_reading;
@@ -563,4 +519,3 @@ int main(void) {
   }
   return 0;
 }
-
